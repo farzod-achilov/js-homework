9f5054606642ad204f47620f5411071b4492d206
Farzod <107624021+farzod-achilov@users.noreply.github.com>
2022-08-16 12:14:15 +0500

    fixed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var elForm = document.querySelector("[data-money-form]");
-var elMoney = document.querySelector("[data-select-amount]");
-var elCurrency = document.querySelector("[data-select-currecy]");
+var elMoney = document.querySelector("[data-input-amount]");
+var elCurrency = document.querySelector("[data-select-currency]");
 var elCalculatedMoney = document.querySelector("[data-text-calculated-money]");
 
 var ONE_USD = 10925;
@@ -15,4 +15,17 @@ elForm.addEventListener("submit", function (evt) {
   var calculatedMoney;
 
   console.log(moneyAmount, currecy);
+
+  if (currecy === "USD") {
+    calculatedMoney = moneyAmount / ONE_USD;
+  }
+  if (currecy === "RUBL") {
+    calculatedMoney = moneyAmount / ONE_RUBL;
+  }
+  if (currecy === "EURO") {
+    calculatedMoney = moneyAmount / ONE_EURO;
+  }
+
+  console.log(calculatedMoney);
+  elCalculatedMoney.textContent = `${calculatedMoney.toFixed(2)} ${currecy}`;
 });
